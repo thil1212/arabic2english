aa7bf10058d05a94c0d199299936fc8f89f7026d
thil_1212 <senthilkumar.srinivasan@yahoo.in>
2015-06-12 23:31:51 +0530

    committing the changes to support 10^1 digits

--- lib/arabic2english.rb
+++ lib/arabic2english.rb
@@ -2,10 +2,10 @@ require "arabic2english/version"
 
 module Arabic2english
   @required_numerals = Hash[1=>:one, 2=>:two, 3=>:three, 4=>:four, 5=>:five, 6=>:six, 7=>:seven, 8=>:eight, 
-                                    9=>:nine, 10=> :ten,11=>:eleven, 12=> :tweleve, 13=>:thirteen, 14=>:fourteen, 
-                                    15=>:fifteen, 16=>:sixteen, 17=> :seventeen, 18=>:eighteen, 19=>:nineteen, 20=>:tweenty,
-                                    30=>:thireety, 40=>:fourty, 50=>:fifty, 60=>:sixty,70=>:seventy, 80=>:eighty, 90=>:ninety,
-                                    100=>:hundred, 1000=>:thousand, 10**6=>:milion, 10**9=> :bilion, 10**12=> :trilion]
+                                    9=>:nine, 10=> :ten,11=>:eleven, 12=> :twelve, 13=>:thirteen, 14=>:fourteen, 
+                                    15=>:fifteen, 16=>:sixteen, 17=> :seventeen, 18=>:eighteen, 19=>:nineteen, 20=>:twenty,
+                                    30=>:thirty, 40=>:forty, 50=>:fifty, 60=>:sixty,70=>:seventy, 80=>:eighty, 90=>:ninety,
+                                    100=>:hundred, 1000=>:thousand, 10**6=>:million, 10**9=> :billion, 10**12=> :trillion]
 
 
  
@@ -13,11 +13,15 @@ module Arabic2english
  	 no_of_digits = arabic_numbers.to_s.split('').length - 1
 
  	 case no_of_digits
-
  	   when 0 
- 	    return @required_numerals[arabic_numbers].to_s;
-
+ 	     @required_numerals[arabic_numbers].to_s;
  	   when 1
+        if arabic_numbers % 10 == 0 ||  arabic_numbers < 20
+        	@required_numerals[arabic_numbers].to_s 
+        else
+        	reminder = arabic_numbers % 10;
+        	@required_numerals[arabic_numbers - reminder].to_s + " "+converter(reminder)
+        end
  	   when 2
  	   when 3
  	   when 4
--- spec/arabic2english_spec.rb
+++ spec/arabic2english_spec.rb
@@ -4,20 +4,23 @@ describe Arabic2english do
 
   it "Should support for 10^0" do
   	(Arabic2english::converter 5).should == 'five'
-
   end
 
-  it "Should support for 10^1"
+  it "Should support for 10^1" do
+  	(Arabic2english::converter 77).should == 'seventy seven'
+  end
+
+
   it "should support for 10^2"
   it "should support for 10^3"
   it "should support for 10^4"
   it "should support for 10^5"
-  it "should support for 10^6 is 1 milion"
+  it "should support for 10^6 is 1 million"
   it "should support for 10^7"
   it "should support for 10^8"
-  it "should support for 10^9 is 1 bilion"
+  it "should support for 10^9 is 1 billion"
   it "should support for 10^10 "
   it "should support for 10^11"
-  it "should support for 10^12 is 1 trilion"
+  it "should support for 10^12 is 1 trillion"
 
 end

31f5b89cba45dc1298cfed0c2654273d54e470e2
thil_1212 <senthilkumar.srinivasan@yahoo.in>
2015-06-12 21:08:06 +0530

    added arabic2english_spec.rb

---
+++ spec/arabic2english_spec.rb
@@ -0,0 +1,19 @@
+require_relative '../lib/arabic2english'
+
+describe Arabic2english do
+
+  it "Should support for 10^0"
+  it "Should support for 10^1"
+  it "should support for 10^2"
+  it "should support for 10^3"
+  it "should support for 10^4"
+  it "should support for 10^5"
+  it "should support for 10^6 is 1 milion"
+  it "should support for 10^7"
+  it "should support for 10^8"
+  it "should support for 10^9 is 1 bilion"
+  it "should support for 10^10 "
+  it "should support for 10^11"
+  it "should support for 10^12 is 1 trilion"
+
+end
